41994d2f9a200376183778b9816b9d44304b1df4
Vincent <weisunglee@gmail.com>
2014-06-22 21:29:41 +0800

    Add practice 01.

---
+++ 01/hello_ruby.rb
@@ -0,0 +1,8 @@
+puts "Hello world"
+puts "Hello again"
+puts "I like this typing this"
+puts "This is fun."
+puts "I'd much rather you 'not'. "
+puts 'I "Said" do not touch this.'
+
+# this is comment
