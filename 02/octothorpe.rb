4b5c40ac2e5cedf1dba4d3e11b628c81c66eaafe
Vincent <weisunglee@gmail.com>
2014-06-22 21:47:17 +0800

    Add octothorpe (comment) practice.

---
+++ 02/octothorpe.rb
@@ -0,0 +1,9 @@
+# A comment, this is so you can read your program later.
+# Anthing after the # is ignore by Ruby.
+
+puts "I could have code like this." #and the comment after is ignore
+
+#You can also use a comment to "disable" or comment out a piece of code:
+#print "This won't run."
+
+puts "This will run."
