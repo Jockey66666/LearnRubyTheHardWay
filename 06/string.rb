c0709ccc98082f308980c2bea7233674e1d73260
Vincent <weisunglee@gmail.com>
2014-06-23 23:09:29 +0800

    Add string practice

---
+++ 06/string.rb
@@ -0,0 +1,24 @@
+x = "There are #{10} types of people."
+
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}."
+
+puts x
+puts y
+
+puts "I said: #{x}."
+puts "I also said: '#{y}'."
+
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+puts joke_evaluation
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+puts w + e
+
+#puts "There are %d types of people." % 10
+#puts "There are %s types of people." % 10   對Ruby來說這兩行是一樣的
