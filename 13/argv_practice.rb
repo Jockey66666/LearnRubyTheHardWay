9031a47fb914fbb13fe29df042e18223419dfa36
Vincent <weisunglee@gmail.com>
2014-06-26 21:57:04 +0800

    add argv practice

---
+++ 13/argv_practice.rb
@@ -0,0 +1,6 @@
+first, second, third = ARGV
+
+puts "The script is called: #{$0}" #等於argv[0]
+puts "Your first variable is: #{first}" #argv[1]
+puts "Your second variable is: #{second}" #argv[2]
+puts "Your third variable is: #{third}" #argv[3]
